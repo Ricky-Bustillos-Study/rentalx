4987a70d48f3147807ce9b811b0d324592001498
Ricky Bustillos <henrique.ssbl@hotmail.com>
2022-02-10 17:26:50 -0300

    feat(category): repository & get

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,25 +1,24 @@
 import { Request, Response, Router } from 'express';
 
-import { Category } from '../models/Category';
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
+const categoriesRepository: CategoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (request: Request, response: Response) => {
   const { name, description } = request.body;
 
   const category: Category = new Category();
 
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date(),
-  });
-
-  categories.push(category);
+  categoriesRepository.create({ name, description });
 
   return response.status(201).send();
+});
+
+categoriesRoutes.get('/', (request: Request, response: Response) => {
+  const categories = categoriesRepository.getAll();
+
+  return response.status(200).json(categories);
 });
 
 export { categoriesRoutes };
