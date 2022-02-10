a6db0d39a86062544e6c9a5c06534cb6e2812f28
Ricky Bustillos <henrique.ssbl@hotmail.com>
2022-02-10 14:44:11 -0300

    feat(category): create, type and route

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,25 @@
+import { Request, Response, Router } from 'express';
+
+import { Category } from '../models/Category';
+
+const categoriesRoutes = Router();
+
+const categories: Category[] = [];
+
+categoriesRoutes.post('/', (request: Request, response: Response) => {
+  const { name, description } = request.body;
+
+  const category: Category = new Category();
+
+  Object.assign(category, {
+    name,
+    description,
+    created_at: new Date(),
+  });
+
+  categories.push(category);
+
+  return response.status(201).send();
+});
+
+export { categoriesRoutes };
