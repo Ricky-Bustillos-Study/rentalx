36ccfe3eb3d94455bca4e99531243dac31b10809
Ricky Bustillos <henrique.ssbl@hotmail.com>
2022-03-01 02:48:57 -0300

    feature(category): applyed use case pattern & controller

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,22 +1,14 @@
 import { Request, Response, Router } from 'express';
 
 import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
-import { CreateCategoryService } from '../modules/cars/services/CreateCategoryService';
+import { createCategoryController } from '../modules/cars/useCases/createCategory';
 
 const categoriesRoutes: Router = Router();
 const categoriesRepository: CategoriesRepository = new CategoriesRepository();
 
-categoriesRoutes.post('/', (request: Request, response: Response) => {
-  const { name, description } = request.body;
+categoriesRoutes.post('/', (request, response) => createCategoryController.handle(request, response));
 
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-
-  createCategoryService.execute({ name, description });
-
-  return response.status(201).send();
-});
-
-categoriesRoutes.get('/', (request: Request, response: Response) => {
+categoriesRoutes.get('/', (request, response) => {
   const categories = categoriesRepository.getAll();
 
   return response.status(200).json(categories);
