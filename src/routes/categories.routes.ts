dc79340cff81ccc78d6daa586f8c8dce81406974
Ricky Bustillos <henrique.ssbl@hotmail.com>
2022-03-01 19:16:47 -0300

    feat(category): added multer for CSV upload

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,12 +1,21 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 import { getCategoriesController } from '../modules/cars/useCases/getCategories';
+import { importCategoryController } from '../modules/cars/useCases/importCategory';
 
 const categoriesRoutes: Router = Router();
+
+const multerOptions: multer.Options = {
+  dest: './tmp',
+};
+const upload = multer(multerOptions);
 
 categoriesRoutes.post('/', (request, response) => createCategoryController.handle(request, response));
 
 categoriesRoutes.get('/', (request, response) => getCategoriesController.handle(request, response));
+
+categoriesRoutes.post('/import', upload.single('file'), (request, response) => importCategoryController.handle(request, response));
 
 export { categoriesRoutes };
