00143e776355a905aee60b5fa5e1afc59b716cc2
Ricky Bustillos <henrique.ssbl@hotmail.com>
2022-03-01 04:04:22 -0300

    feat(specifications): added use case & controller

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,19 +1,9 @@
 import { Request, Response, Router } from 'express';
-import { SpecificationsRepository } from '../modules/cars/repositories/implementations/SpecificationsRepository';
-import { CreateSpecificationService } from '../modules/cars/services/CreateSpecificationService';
+
+import { createSpecificationController } from '../modules/cars/useCases/createSpecification';
 
 const specificationsRoutes = Router();
 
-const specificationsRepository = new SpecificationsRepository();
-
-specificationsRoutes.post('/', (request: Request, response: Response) => {
-  const { description, name } = request.body;
-
-  const createSpecificationService = new CreateSpecificationService(specificationsRepository);
-
-  createSpecificationService.execute({ description, name });
-
-  return response.status(201).send();
-});
+specificationsRoutes.post('/', (request: Request, response: Response) => createSpecificationController.handle(request, response));
 
 export { specificationsRoutes };
