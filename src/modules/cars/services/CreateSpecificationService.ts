7fae4903cffaecd599e231a1a35688327368a641
Ricky Bustillos <henrique.ssbl@hotmail.com>
2022-03-01 01:54:40 -0300

    feat(specifications): cars specifications module

---
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -0,0 +1,23 @@
+import { ISpecificationsRepository } from '../repositories/ISpecificationsRepository';
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+export class CreateSpecificationService {
+  constructor(private specificationsRepository: ISpecificationsRepository) {}
+
+  execute({ description, name }: IRequest): void {
+    const specificationAlreadyExists = this.specificationsRepository.findByName(name);
+
+    if (specificationAlreadyExists) {
+      throw new Error('Category already exists!');
+    }
+
+    this.specificationsRepository.create({
+      description,
+      name,
+    });
+  }
+}
